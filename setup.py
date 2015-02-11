71acbc4315cac1dd4091cc81148f485b0f596342
Matthias Kümmerer <matthias@matthias-k.org>
2015-02-11 17:42:55 +0100

    Adapt setup.py for Ubuntu
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ from setuptools import setup
 from setuptools.extension import Extension
 from distutils.sysconfig import get_python_lib
 from Cython.Distutils import build_ext
+import Cython.Distutils
+import Cython.Compiler.Options
 import numpy as np
 import os.path
 import sys
@@ -55,36 +57,29 @@ def main_win32():
 
 def pkgconfig(*packages, **kw):
     """Based on http://code.activestate.com/recipes/502261-python-distutils-pkg-config/"""
-    
+
     import commands
-    
+
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
     for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
         kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-        
+
     kw['include_dirs'] += [np.get_include()]
-        
+
     return kw
 
 
 def main_unix():
-    setup(
-        name=PACKAGE_NAME,
-        version=VERSION,
-        description=DESCRIPTION,
-        author=AUTHOR,
-        author_email=EMAIL,
-        url=URL,
-        packages=[PACKAGE_NAME],
-        cmdclass = {'build_ext': build_ext},
-        ext_modules = [
-            Extension(
-                PACKAGE_NAME + '.' + 'cyipopt',
-                ['src/cyipopt.pyx'],
-                **pkgconfig('ipopt')
-            )
-        ],
-    )
+    setup(name = 'ipopt',
+          packages = ['ipopt'],
+          cmdclass = {'build_ext': Cython.Distutils.build_ext},
+          include_package_data=True,
+          ext_modules = [Extension("cyipopt",
+                                   ['src/cyipopt.pyx'],
+                                   **pkgconfig('ipopt')
+                                   )
+                        ]
+         )
 
 
 if __name__ == '__main__':
