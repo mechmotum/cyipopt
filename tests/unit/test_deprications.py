cb42804dc3ac76def234ad83f457fedf89848e1a
Sam Brockie <sgb39@cam.ac.uk>
2020-07-02 10:23:24 +0100

    Add placeholders for deprication test functions

---
+++ tests/unit/test_deprications.py
@@ -0,0 +1,17 @@
+import pytest
+
+import cyipopt
+
+
+
+
+def test_ipopt_import_deprication():
+	pass
+
+
+def test_non_pep8_class_name_deprication():
+	pass
+
+
+def test_non_pep8_method_names_deprication():
+	pass
