138b6cb818cecc1728c8b5bce6411a021a1c5f77
Sam Brockie <sgb39@cam.ac.uk>
2020-07-02 10:15:45 +0100

    Handle backcompatibility with module name change from 'ipopt' to 'cyipopt'

---
+++ ipopt/__init__.py
@@ -0,0 +1,15 @@
+import sys
+import warnings
+
+sys.modules[__name__] = __import__("cyipopt")
+
+
+def issue_deprication_warning():
+	"""Warn user of deprication of 'ipopt' as module name."""
+	msg = ("The module has been renamed to 'cyipopt' from 'ipopt'. Please "
+		"import using 'import cyipopt' and remove all uses of 'import ipopt' "
+		"in your code as this will be depricated in a future release.")
+	warnings.warn(msg, FutureWarning)
+
+
+issue_deprication_warning()
