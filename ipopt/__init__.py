919a3c934c79ef36acd1d25befb73f5148b0b6d1
Sam Brockie <sgb39@cam.ac.uk>
2020-07-02 14:40:19 +0100

    Fix spelling

--- ipopt/__init__.py
+++ ipopt/__init__.py
@@ -4,12 +4,12 @@ import warnings
 sys.modules[__name__] = __import__("cyipopt")
 
 
-def issue_deprication_warning():
+def issue_deprecation_warning():
 	"""Warn user of deprication of 'ipopt' as module name."""
 	msg = ("The module has been renamed to 'cyipopt' from 'ipopt'. Please "
 		"import using 'import cyipopt' and remove all uses of 'import ipopt' "
-		"in your code as this will be depricated in a future release.")
+		"in your code as this will be deprecated in a future release.")
 	warnings.warn(msg, FutureWarning)
 
 
-issue_deprication_warning()
+issue_deprecation_warning()
