ed97fa1ca59d9334b82c3eb178cce4b0cb980d94
Sam Brockie <sgb39@cam.ac.uk>
2020-09-26 18:56:44 +0100

    Remove shebang

---
+++ ipopt/version.py
@@ -0,0 +1,14 @@
+"""
+cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
+
+Copyright (C) 2012-2015 Amit Aides
+Copyright (C) 2015-2017 Matthias Kümmerer
+Copyright (C) 2017-2020 cyipopt developers
+
+Author: Matthias Kümmerer <matthias.kuemmerer@bethgelab.org>
+(original Author: Amit Aides <amitibo@tx.technion.ac.il>)
+URL: https://github.com/matthias-k/cyipopt
+License: EPL 1.0
+"""
+
+__version__ = "1.0.0"
