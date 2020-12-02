30d9fd8ee2ef0ff408f9628763b20dd06604537d
Sam Brockie <sgb39@cam.ac.uk>
2020-12-02 14:02:12 +0000

    Convert indentation to spaces

--- ipopt/__init__.py
+++ ipopt/__init__.py
@@ -5,11 +5,12 @@ sys.modules[__name__] = __import__("cyipopt")
 
 
 def issue_deprecation_warning():
-	"""Warn user of deprication of 'ipopt' as module name."""
-	msg = ("The module has been renamed to 'cyipopt' from 'ipopt'. Please "
-		"import using 'import cyipopt' and remove all uses of 'import ipopt' "
-		"in your code as this will be deprecated in a future release.")
-	warnings.warn(msg, FutureWarning)
+    """Warn user of deprication of 'ipopt' as module name."""
+    msg = ("The module has been renamed to 'cyipopt' from 'ipopt'. Please "
+           "import using 'import cyipopt' and remove all uses of "
+           "'import ipopt' in your code as this will be deprecated in a "
+           "future release.")
+    warnings.warn(msg, FutureWarning)
 
 
 issue_deprecation_warning()
