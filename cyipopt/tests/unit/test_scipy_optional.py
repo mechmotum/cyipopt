69f347ea79f9049fec616a617853ef7c68b4edbd
Sam Brockie <sgb39@cam.ac.uk>
2020-12-02 12:47:47 +0000

    Add docstrings, add further assertions to minimize_ipopt test

--- cyipopt/tests/unit/test_scipy_optional.py
+++ cyipopt/tests/unit/test_scipy_optional.py
@@ -9,6 +9,7 @@ which requires SciPy.
 import re
 import sys
 
+import numpy as np
 import pytest
 
 import cyipopt
@@ -17,6 +18,7 @@ import cyipopt
 @pytest.mark.skipif("scipy" in sys.modules,
                     reason="Test only valid if no Scipy available.")
 def test_minimize_ipopt_import_error_if_no_scipy():
+    """`minimize_ipopt` not callable without SciPy installed."""
     expected_error_msg = re.escape("Install SciPy to use the "
                                    "`minimize_ipopt` function.")
     with pytest.raises(ImportError, match=expected_error_msg):
@@ -26,7 +28,12 @@ def test_minimize_ipopt_import_error_if_no_scipy():
 @pytest.mark.skipif("scipy" not in sys.modules,
                     reason="Test only valid if Scipy available.")
 def test_minimize_ipopt_if_scipy():
+    """If SciPy is installed `minimize_ipopt` works correctly."""
     from scipy.optimize import rosen, rosen_der
     x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
     res = cyipopt.minimize_ipopt(rosen, x0, jac=rosen_der)
     assert isinstance(res, dict)
+    assert np.isclose(res.get("fun") , 0)
+    assert res.get("status") == 0
+    assert res.get("success") == True
+    np.testing.assert_allclose(res.get("x"), np.ones(5))
