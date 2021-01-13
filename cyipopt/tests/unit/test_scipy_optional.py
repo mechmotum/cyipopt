49f8bb877780e2a55d8815daa28b58666c25e603
Jason K. Moore <moorepants@gmail.com>
2021-01-13 14:49:14 +0100

    Update cyipopt/tests/unit/test_scipy_optional.py
    Co-authored-by: Sam Brockie <sgb39@cam.ac.uk>

--- cyipopt/tests/unit/test_scipy_optional.py
+++ cyipopt/tests/unit/test_scipy_optional.py
@@ -45,7 +45,7 @@ def test_minimize_ipopt_nojac_if_scipy():
     """If SciPy is installed `minimize_ipopt` works correctly."""
     from scipy.optimize import rosen
     x0 = [1.3, 0.7, 0.8, 1.9, 1.2]
-    options={'tol':1e-7}
+    options = {"tol": 1e-7}
     res = cyipopt.minimize_ipopt(rosen, x0,options=options)
     assert isinstance(res, dict)
     assert np.isclose(res.get("fun") , 0)
