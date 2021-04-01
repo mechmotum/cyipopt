867d94ff73a4f4b2e12d8cb7c6e8d397a47f3b17
Jason K. Moore <moorepants@gmail.com>
2021-04-01 16:50:44 +0200

    Adjusted copyright statements in the files.

--- cyipopt/__init__.py
+++ cyipopt/__init__.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
+
+Copyright (C) 2012-2015 Amit Aides
+Copyright (C) 2015-2017 Matthias Kümmerer
+Copyright (C) 2017-2021 cyipopt developers
+
+License: EPL 1.0
+"""
 
 from __future__ import absolute_import
 
--- cyipopt/cython/ipopt_wrapper.pyx
+++ cyipopt/cython/ipopt_wrapper.pyx
@@ -4,11 +4,8 @@ cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
 
 Copyright (C) 2012-2015 Amit Aides
 Copyright (C) 2015-2017 Matthias Kümmerer
-Copyright (C) 2017-2020 cyipopt developers
+Copyright (C) 2017-2021 cyipopt developers
 
-Author: Matthias Kümmerer <matthias.kuemmerer@bethgelab.org>
-(original Author: Amit Aides <amitibo@tx.technion.ac.il>)
-URL: https://github.com/matthias-k/cyipopt
 License: EPL 1.0
 """
 
