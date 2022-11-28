dd271c6ce7bd6fd33e90c2a04d639cf82282f8b0
Jason K. Moore <moorepants@gmail.com>
2022-11-28 14:58:42 +0100

    Update remaining copyright dates.

--- examples/hs071.py
+++ examples/hs071.py
@@ -4,7 +4,7 @@ cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
 
 Copyright (C) 2012-2015 Amit Aides
 Copyright (C) 2015-2017 Matthias Kümmerer
-Copyright (C) 2017-2021 cyipopt developers
+Copyright (C) 2017-2022 cyipopt developers
 
 License: EPL 1.0
 """
