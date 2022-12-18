1a144c99747a94013d633a6b5816bf6916e599c7
Jonathan Helgert <jhelgert@users.noreply.github.com>
2022-12-18 08:46:39 +0100

    Update cyipopt/scipy_interface.py
    Co-authored-by: Jason K. Moore <moorepants@gmail.com>

--- cyipopt/scipy_interface.py
+++ cyipopt/scipy_interface.py
@@ -255,7 +255,7 @@ def get_constraint_dimensions(constraints, x0):
     if isinstance(constraints, dict):
         constraints = (constraints, )
     for con in constraints:
-        if con.get('jac', False) == True:
+        if con.get('jac', False):
             m = len(np.atleast_1d(con['fun'](x0, *con.get('args', []))[0]))
         else:
             m = len(np.atleast_1d(con['fun'](x0, *con.get('args', []))))
