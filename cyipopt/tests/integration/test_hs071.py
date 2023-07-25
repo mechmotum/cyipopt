2eea9256a27a238c6fe3b0d93670d87515e3b995
Robert Parker <robbybparker@gmail.com>
2023-07-25 09:03:23 -0600

    test when evaluation error raised by constraint, Jacobian, and Hessian

--- cyipopt/tests/integration/test_hs071.py
+++ cyipopt/tests/integration/test_hs071.py
@@ -112,7 +112,139 @@ def test_hs071_grad_obj_eval_error(
     cu = hs071_constraint_upper_bounds_fixture
 
     problem = _make_problem(definition, lb, ub, cl, cu)
+    # Solve fails when the evaluation error occurs in objective
+    # gradient evaluation.
     _assert_solve_fails(problem, x0)
 
     # Since we fail at the first evaluation error, we know we only encountered one.
     assert gradient_with_error.n_eval_error == 1
+
+
+def test_hs071_constraints_eval_error(
+    hs071_initial_guess_fixture,
+    hs071_problem_instance_fixture,
+    hs071_definition_instance_fixture,
+    hs071_variable_lower_bounds_fixture,
+    hs071_variable_upper_bounds_fixture,
+    hs071_constraint_lower_bounds_fixture,
+    hs071_constraint_upper_bounds_fixture,
+):
+    class ConstraintsWithError:
+        def __init__(self):
+            self.n_eval_error = 0
+
+        def __call__(self, x):
+            if x[0] > 1.1:
+                self.n_eval_error += 1
+                raise cyipopt.CyIpoptEvaluationError()
+            return np.array((np.prod(x), np.dot(x, x)))
+
+    constraints_with_error = ConstraintsWithError()
+
+    x0 = hs071_initial_guess_fixture
+    definition = hs071_definition_instance_fixture
+    definition.constraints = constraints_with_error
+    definition.intermediate = None
+
+    lb = hs071_variable_lower_bounds_fixture
+    ub = hs071_variable_upper_bounds_fixture
+    cl = hs071_constraint_lower_bounds_fixture
+    cu = hs071_constraint_upper_bounds_fixture
+
+    problem = _make_problem(definition, lb, ub, cl, cu)
+    _solve_and_assert_correct(problem, x0)
+
+    assert constraints_with_error.n_eval_error > 0
+
+
+def test_hs071_jacobian_eval_error(
+    hs071_initial_guess_fixture,
+    hs071_problem_instance_fixture,
+    hs071_definition_instance_fixture,
+    hs071_variable_lower_bounds_fixture,
+    hs071_variable_upper_bounds_fixture,
+    hs071_constraint_lower_bounds_fixture,
+    hs071_constraint_upper_bounds_fixture,
+):
+    class JacobianWithError:
+        def __init__(self):
+            self.n_eval_error = 0
+
+        def __call__(self, x):
+            if x[0] > 1.1:
+                self.n_eval_error += 1
+                raise cyipopt.CyIpoptEvaluationError()
+            return np.concatenate((np.prod(x) / x, 2 * x))
+
+    jacobian_with_error = JacobianWithError()
+
+    x0 = hs071_initial_guess_fixture
+    definition = hs071_definition_instance_fixture
+    definition.jacobian = jacobian_with_error
+    definition.intermediate = None
+
+    lb = hs071_variable_lower_bounds_fixture
+    ub = hs071_variable_upper_bounds_fixture
+    cl = hs071_constraint_lower_bounds_fixture
+    cu = hs071_constraint_upper_bounds_fixture
+
+    problem = _make_problem(definition, lb, ub, cl, cu)
+    # Solve fails when the evaluation error occurs in constraint
+    # Jacobian evaluation.
+    _assert_solve_fails(problem, x0)
+
+    assert jacobian_with_error.n_eval_error == 1
+
+
+def test_hs071_hessian_eval_error(
+    hs071_initial_guess_fixture,
+    hs071_problem_instance_fixture,
+    hs071_definition_instance_fixture,
+    hs071_variable_lower_bounds_fixture,
+    hs071_variable_upper_bounds_fixture,
+    hs071_constraint_lower_bounds_fixture,
+    hs071_constraint_upper_bounds_fixture,
+):
+    class HessianWithError:
+        def __init__(self):
+            self.n_eval_error = 0
+
+        def __call__(self, x, lagrange, obj_factor):
+            if x[0] > 1.1:
+                self.n_eval_error += 1
+                raise cyipopt.CyIpoptEvaluationError()
+
+            H = obj_factor * np.array([
+                (2 * x[3], 0, 0, 0),
+                (x[3], 0, 0, 0),
+                (x[3], 0, 0, 0),
+                (2 * x[0] + x[1] + x[2], x[0], x[0], 0),
+            ])
+            H += lagrange[0] * np.array([
+                (0, 0, 0, 0),
+                (x[2] * x[3], 0, 0, 0),
+                (x[1] * x[3], x[0] * x[3], 0, 0),
+                (x[1] * x[2], x[0] * x[2], x[0] * x[1], 0),
+            ])
+            H += lagrange[1] * 2 * np.eye(4)
+            row, col = np.nonzero(np.tril(np.ones((4, 4))))
+            return H[row, col]
+
+    hessian_with_error = HessianWithError()
+
+    x0 = hs071_initial_guess_fixture
+    definition = hs071_definition_instance_fixture
+    definition.hessian = hessian_with_error
+    definition.intermediate = None
+
+    lb = hs071_variable_lower_bounds_fixture
+    ub = hs071_variable_upper_bounds_fixture
+    cl = hs071_constraint_lower_bounds_fixture
+    cu = hs071_constraint_upper_bounds_fixture
+
+    problem = _make_problem(definition, lb, ub, cl, cu)
+    # Solve fails when the evaluation error occurs in Lagrangian
+    # Hessian evaluation.
+    _assert_solve_fails(problem, x0)
+
+    assert hessian_with_error.n_eval_error == 1
