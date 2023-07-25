17eb332b19fdde1033c621c671bed70f0e306d7d
Robert Parker <robbybparker@gmail.com>
2023-07-25 09:05:20 -0600

    add copyright

--- cyipopt/exceptions.py
+++ cyipopt/exceptions.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+cyipopt: Python wrapper for the Ipopt optimization package, written in Cython.
+
+Copyright (C) 2012-2015 Amit Aides
+Copyright (C) 2015-2017 Matthias Kümmerer
+Copyright (C) 2017-2023 cyipopt developers
+
+License: EPL 2.0
+"""
+
 class CyIpoptEvaluationError(ArithmeticError):
     """An exception that should be raised in evaluation callbacks to signal
     to CyIpopt that a numerical error occured during function evaluation.
